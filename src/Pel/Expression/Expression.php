77609592128696d750da0ade89122bf38d3ad3b3
Benjamin Dulau <benjamin.dulau@anonymation.com>
2013-04-12 17:45:08 +0200

    Initial commit

---
+++ src/Pel/Expression/Expression.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of php-expression
+ *
+ * (c) Kitano <contact@kitanolabs.org>
+ *
+ */
+
+namespace Pel\Expression;
+
+class Expression
+{
+
+}
---
+++ tests/Pel/Tests/Expression/ExpressionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of php-expression
+ *
+ * (c) Kitano <contact@kitanolabs.org>
+ *
+ */
+
+namespace Pel\Tests\Expression;
+
+class ExpressionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}

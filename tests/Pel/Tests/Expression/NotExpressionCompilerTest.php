679b3b92edddb8bd38cba33265a37ad851340eaf
Boris Guéry <guery.b@gmail.com>
2013-04-17 20:53:14 +0200

    Added NotExpressionCompilerTest

---
+++ tests/Pel/Tests/Expression/NotExpressionCompilerTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: bguery
+ * Date: 17/04/13
+ * Time: 20:37
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Pel\Tests\Expression;
+
+
+use Pel\Expression\Expression;
+use Pel\Expression\ExpressionCompiler;
+
+class NotExpressionCompilerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getFalseExpressions
+     */
+    public function testNotExpressionCompiler($expr, $expectedResult)
+    {
+        $evaluator = eval(
+            $source = $this->compiler->compileExpression(
+                new Expression($expr)
+            )
+        );
+
+        $this->assertSame($expectedResult, $evaluator(array()));
+    }
+
+    public function getFalseExpressions()
+    {
+        return array(
+            array('not "a string is always true"', false),
+            // (except if it is empty)
+            array('not ""', true),
+            // zero as string is evaluated to false
+            array('not "0"', true),
+            // but "1" is true
+            array('not "1"', false),
+        );
+    }
+
+    protected function setUp()
+    {
+        $this->compiler = new ExpressionCompiler();
+    }
+}
